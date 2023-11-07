054b9acf55247a10d6c3a55e125bcb6f791bd7d2
Aruthsha Induwara Dewasurendra <aruthshadew@gmail.com>
2023-11-07 14:48:07 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+   <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <a href="index.php" class="navbar-brand ps-3">Registration and Login System</a>
+   </nav>
+
+   <div id="layoutSidenav">
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid px-4">
+                <h1 class="mt-4">
+                    User Registration & Login System Management
+                </h1>
+                <ol class="breadcrumb mb-4">
+                    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                </ol>
+                <div class="row">
+                    <div class="col-xl-4 col-md-6">
+                        <div class="card bg-primary text-white mb-4">
+                            <div class="card-body">
+                                Not Registered Yet
+                            </div>
+                            <div class="card-footer d-flex align-items-center justify-content-between">
+                                <a href="signup.php" class="small text-white stratched-link">Signup Here</a>
+                                <div class="small text-white"><i class="fa-solid fa-angle-right"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="card bg-primary text-white mb-4">
+                            <div class="card-body">
+                                Not Registered Yet
+                            </div>
+                            <div class="card-footer d-flex align-items-center justify-content-between">
+                                <a href="signup.php" class="small text-white stratched-link">Signup Here</a>
+                                <div class="small text-white"><i class="fa-solid fa-angle-right"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="card bg-primary text-white mb-4">
+                            <div class="card-body">
+                                Not Registered Yet
+                            </div>
+                            <div class="card-footer d-flex align-items-center justify-content-between">
+                                <a href="signup.php" class="small text-white stratched-link">Signup Here</a>
+                                <div class="small text-white"><i class="fa-solid fa-angle-right"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+   </div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
